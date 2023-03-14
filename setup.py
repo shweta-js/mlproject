c1977b7a4c529eb0f00122585b63d291d7df2804
shweta <shwetajs200@gmail.com>
2023-03-14 15:44:35 +0530

    initial setup

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-from setuptool import find_packages, setup
+from setuptools import find_packages, setup
 from typing import List
 
+HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
     ''' 
     this function will return the list of requirements
     '''
     requirements=[]
+ 
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
